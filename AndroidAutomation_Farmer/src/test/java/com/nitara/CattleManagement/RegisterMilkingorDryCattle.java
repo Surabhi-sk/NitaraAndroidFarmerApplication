b3cb9613657ef8475be53fac3d6955c06231a6de
Eesha Martis <89638054+eesha-m@users.noreply.github.com>
2022-06-02 10:42:39 +0530

    Update RegisterMilkingorDryCattle.java
    Added regression testcases

--- AndroidAutomation_Farmer/src/test/java/com/nitara/CattleManagement/RegisterMilkingorDryCattle.java
+++ AndroidAutomation_Farmer/src/test/java/com/nitara/CattleManagement/RegisterMilkingorDryCattle.java
@@ -6,6 +6,284 @@ import com.nitara.utils.DataProviderUtils;
 import appCommonClasses.GenericBase;
 
 public class RegisterMilkingorDryCattle extends GenericBase{
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_TagnumberAcceptsSpecialChars(Map<String,String> data) throws Exception{		
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number with special chars and save */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(data.get("tagNumber"));		
+		registerMilkingCattlePage.press_SaveButton();	
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_DuplicateTagnumberAllowed(Map<String,String> data) throws Exception{	
+		
+		String calving_date = generateRandomData.getPastDate(0);
+		
+		/** Register cattle */
+		String Tag = helperFunctions.RegisterCattle_MilkingCattle();		
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter previously registered Tag number and calving date and save */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(Tag);		
+		registerMilkingCattlePage.enter_Calvingdate(calving_date);
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression1",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_CoopTagnumberAcceptsNonnumericChars(Map<String,String> data) throws Exception{
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, Non-numeric Coop Tag Number and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(data.get("tagNumber"));	
+		registerMilkingCattlePage.enter_CoopTagNumber(data.get("coopTagNumber"));
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_CoopTagnumberAcceptsLessDigits(Map<String,String> data) throws Exception{
+
+		String cooptagNumber =generateRandomData.generateRandomNumber(11);
+		
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, 11 digit Coop Tag Number and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(data.get("tagNumber"));	
+		registerMilkingCattlePage.enter_CoopTagNumber(cooptagNumber);
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_CoopTagnumberAcceptsMoreDigits(Map<String,String> data) throws Exception{
+
+		String cooptagNumber =generateRandomData.generateRandomNumber(13);
+		
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, 11 digit Coop Tag Number and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(data.get("tagNumber"));	
+		registerMilkingCattlePage.enter_CoopTagNumber(cooptagNumber);
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_TagnumberMandate(Map<String,String> data) throws Exception{
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Save without entering any field  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.press_SaveButton();	
+		//registerMilkingCattlePage.press_SuggestionPopup();
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_CalvingDateMandate(Map<String,String> data) throws Exception{
+		
+		String tagNumber =generateRandomData.generateRandomNumber(7);
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(tagNumber);	
+		registerMilkingCattlePage.press_SaveButton();	
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_InseminationDateMandate(Map<String,String> data) throws Exception{
+		
+		String tagNumber =generateRandomData.generateRandomNumber(7);
+		String calving_date = generateRandomData.getPastDate(0);
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, calving date and click on isInseminated toggle and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(tagNumber);	
+		registerMilkingCattlePage.enter_Calvingdate(calving_date);
+		registerMilkingCattlePage.isCattleInseminated();
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_DryPeriodDateMandate(Map<String,String> data) throws Exception{
+		
+		String tagNumber =generateRandomData.generateRandomNumber(7);
+		String calving_date = generateRandomData.getPastDate(0);
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, calving date and click on isDry toggle and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(tagNumber);	
+		registerMilkingCattlePage.enter_Calvingdate(calving_date);
+		registerMilkingCattlePage.cattleIsDry();
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_CalvingAndInseminationDateGap(Map<String,String> data) throws Exception{
+		
+		String tagNumber =generateRandomData.generateRandomNumber(7);
+		String insemination_date = generateRandomData.getPastDate(0);
+		String calving_date = generateRandomData.getPastDate(14);
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, calving date and insemination date and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(tagNumber);	
+		registerMilkingCattlePage.enter_Calvingdate(calving_date);
+		registerMilkingCattlePage.isCattleInseminated();
+		registerMilkingCattlePage.enter_InseminationDate(insemination_date);
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterMilkingorDryCattle_DryPeriodDateCannotBeBeforeCalvingDate(Map<String,String> data) throws Exception{
+		
+		String tagNumber =generateRandomData.generateRandomNumber(7);
+		String calving_date = generateRandomData.getPastDate(0);
+		String dry_period_date = generateRandomData.getPastDate(1);
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, calving date and dry period date and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(tagNumber);	
+		registerMilkingCattlePage.enter_Calvingdate(calving_date);
+		registerMilkingCattlePage.cattleIsDry();
+		registerMilkingCattlePage.enter_DryPeriodDate(dry_period_date);
+		registerMilkingCattlePage.press_SaveButton();		
+		registerMilkingCattlePage.assertWarningMsg(data.get("warningMessage"));
+		
+	}
+	
+	@Test(groups="Regression")
+	public void RegisterMilkingorDryCattle_DefaultHeatEntryAdded() throws Exception{
+		
+		String tagNumber =generateRandomData.generateRandomNumber(7);
+		String calving_date = generateRandomData.getPastDate(30);
+		String insemination_date = generateRandomData.getPastDate(0);
+
+		/** Farmer Home page - Select Register Cattle */
+		farmerHomePage.waitForPageLoad();
+		farmerHomePage.click_RegisterCattleButton();
+		
+		/** Select Cattle Type page - Select Milking and Dry Cattle */
+		registerMilkingCattlePage.select_cattleType("MILKING AND DRY CATTLE");
+		registerMilkingCattlePage.assert_CattleType();
+		
+		/** Milking and Dry Cattle page - Enter Tag number, calving date and insemination date and save  */
+		farmerHomePage.waitForSmallPageLoad();
+		registerMilkingCattlePage.enter_TagNumber(tagNumber);	
+		registerMilkingCattlePage.enter_Calvingdate(calving_date);
+		registerMilkingCattlePage.isCattleInseminated();
+		registerMilkingCattlePage.enter_InseminationDate(insemination_date);
+		registerMilkingCattlePage.press_SaveButton();
+		
+		/** Go to cattle profile and check if the heat entry is recorded on the breeding timeline */
+		helper_AppNavigation.goTo_CattleProfileSelectActivity(tagNumber,"BREEDING");
+		farmerHomePage.waitForPageLoad();
+		breedingTimelinePage.assert_HeatEntry();
+		
+	}
 
 	@Test(dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
 	public void RegisterCattle_MilkingorDryCattle(Map<String,String> data) throws InterruptedException {
