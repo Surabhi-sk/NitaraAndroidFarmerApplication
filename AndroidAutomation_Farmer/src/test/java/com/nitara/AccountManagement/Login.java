de6c617646102b20a71220c1f57cabb555fe5438
Jinesh Jain <jineshjain132001@gmail.com>
2022-06-24 14:34:13 +0530

    Login

--- AndroidAutomation_Farmer/src/test/java/com/nitara/AccountManagement/Login.java
+++ AndroidAutomation_Farmer/src/test/java/com/nitara/AccountManagement/Login.java
@@ -1,7 +1,10 @@
 package com.nitara.AccountManagement;
 
+import java.util.Map;
+
 import org.testng.annotations.Test;
 
+import com.nitara.utils.DataProviderUtils;
 import com.nitara.utils.PropertyManager;
 
 import appCommonClasses.GenericBase;
@@ -16,7 +19,7 @@ public class Login extends GenericBase{
 		chooseLanguagePage.click_skipBtn(); // Skip Tutorial
 		
 		/** Enter Phone Number **/
-		enterPhoneNoPage.enter_PhoneNo("6352817747");
+		enterPhoneNoPage.enter_PhoneNo("5566556655");
 		enterPhoneNoPage.click_continue();
 		
 		/** Enter PIN */
@@ -25,6 +28,55 @@ public class Login extends GenericBase{
 		
 		/** Navigated to Farmer Home page - assert if user on farmer homepage */
 		farmerHomePage.assert_farmerHomePage();
+	}
+	
+	@Test(dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void Login_PinLessThan6(Map<String,String> data) throws Exception {
+		
+		/** Select Language */
+		chooseLanguagePage.waitForPageLoad();
+		chooseLanguagePage.select_Language("ENGLISH");
+		chooseLanguagePage.click_skipBtn(); // Skip Tutorial
+		
+		/** Enter Phone Number **/
+		enterPhoneNoPage.enter_PhoneNo(data.get("phoneNumber"));
+			enterPhoneNoPage.click_continue();
+		
+		/** Enter PIN */
+		enterPinPage.enter_pin(data.get("pin"));
+		enterPinPage.click_loginBtn();
+		enterPinPage.assertWarning(data.get("warningMessage"));
+	}
+	@Test(dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void Login_PinMandate(Map<String,String> data) throws Exception {
+		
+		/** Select Language */
+		chooseLanguagePage.waitForPageLoad();
+		chooseLanguagePage.select_Language("ENGLISH");
+		chooseLanguagePage.click_skipBtn(); // Skip Tutorial
+		
+		/** Enter Phone Number **/
+		enterPhoneNoPage.enter_PhoneNo(data.get("phoneNumber"));
+		enterPhoneNoPage.click_continue();
+		
+		/** Enter PIN */
+//		enterPinPage.enter_pin(data.get("pin"));
+		enterPinPage.click_loginBtn();
+		enterPinPage.assertWarning(data.get("warningMessage"));
+	}
+	
+	@Test(dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void Login_PhoneMandate(Map<String,String> data) throws Exception {
+		
+		/** Select Language */
+		chooseLanguagePage.waitForPageLoad();
+		chooseLanguagePage.select_Language("ENGLISH");
+		chooseLanguagePage.click_skipBtn(); // Skip Tutorial
+		
+		/** Enter Phone Number **/
+//		enterPhoneNoPage.enter_PhoneNo("5566556655");
+		enterPhoneNoPage.click_continue();
+		enterPinPage.assertWarning(data.get("warningMessage"));
 	}
 
 }
