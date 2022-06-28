2316cbf6d8e67b6ee17a73818c61957fd7ac3656
Jinesh Jain <jineshjain132001@gmail.com>
2022-06-29 00:09:25 +0530

    BCS

--- AndroidAutomation_Farmer/src/test/java/com/nitara/BCSManagement/ViewBcs.java
+++ AndroidAutomation_Farmer/src/test/java/com/nitara/BCSManagement/ViewBcs.java
@@ -8,51 +8,37 @@ import appCommonClasses.GenericBase;
 import appCommonClasses.Helper_AppNavigation;
 import appCommonClasses.HelperFunctions;
 
-public class ViewBcs extends GenericBase 
-
+public class ViewBcs extends GenericBase
 
 {
 	@Test
 	public void ViewBcs() throws Throwable
-	
-	{	
+
+	{
 		/** Register cattle */
 		String url = prop.getProperty("APIbaseUrl");
 		RegisterMilkingCattle reg = new RegisterMilkingCattle();
-		String Tag = reg.registerMilkingOrDryCattle(url,"RegisterMilkingOrDryCattle");
-		
-		
+		String Tag = reg.registerMilkingOrDryCattle(url, "RegisterMilkingOrDryCattle");
+
 		/** Go to cattle Profile page -> Select BCS */
-		new Helper_AppNavigation().goTo_CattleProfileSelectActivity(Tag, "BCS");		
-		
-		
+		new Helper_AppNavigation().goTo_CattleProfileSelectActivity(Tag, "BCS");
+
 		/** Clic Addbcs from viewbcs page */
 		addbcsPage.ClickAddBcs_btn();
-		
-		
+
 		/** Fill bcs Form */
 		String date = generateRandomData.getPastDate(2);
 		addbcsPage.addbcs(date);
-		
+
 //		/** Select Time Period from dropdown */
 //		viewbcspage.select_Timeperiod();
-		
-		/** Assert bcs score*/
-	//	viewbcspage.assert_Bcsscore();
-		
+
+		/** Assert bcs score */
+		 viewbcspage.assert_Bcsscore();
+
 		/** View the added details */
 		viewbcspage.captureScreenshots("View BCS");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
+	
 }
